0641fc29c701d237247a62d32e53aab93c8c406a
xatomen <jorgito899@gmail.com>
2023-10-25 23:19:13 -0300

    Menú 25-10-2023

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int opcion = 0;
+    while(opcion!=0){
+        printf("Menú:\n");
+        printf("1. Agregar proceso\n");
+        printf("0. Salir\n");
+        printf("Ingrese una opción:\n");
+        scanf("%d",&opcion);
+        switch(opcion){
+            case 0:
+            break;
+
+            case 1:
+            break;
+        }
+    }
+    return 0;
+}
