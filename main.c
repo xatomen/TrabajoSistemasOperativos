fd6ca57ab3c02911d729a925db5318f32b4e3d33
Jorge Gallardo <jorgito899@gmail.com>
2023-10-26 11:59:05 -0300

    Se agregan la estructura de datos;falta 26-10-2023

--- main.c
+++ main.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+/*Estructura de datos*/
+
+/*Proceso*/
+typedef struct{
+    int id_proceso;
+    int tamanio_proceso; /*En KiloBytes*/
+    float tiempo_ejecucion; /*En unidades de tiempo*/
+} proceso;
+
+/*Marco de página*/
+typedef struct{
+    int tamanio_marco;
+    int id_proceso_asignado;
+} marco_pagina;
+
+/*Tabla de páginas*/
+typedef marco_pagina tabla_paginas[2048];
+
+typedef proceso cola_procesos[500];
+
+/*Prototipo de funciones*/
+int crear_proceso(int tamanio, float tiempo_ejecucion);
+
+/*Programa principal*/
 int main(){
+    /*Variables*/
     int opcion = 0;
+    tabla_paginas Tabla;
+    cola_procesos Cola; /*USAR FCFS???????????????????????*/
+
+    /*Menú y ejecución del aplicativo*/
     while(opcion!=0){
         printf("Menú:\n");
         printf("1. Agregar proceso\n");
@@ -17,4 +48,20 @@ int main(){
         }
     }
     return 0;
+}
+
+int crear_proceso(int tamanio, float tiempo_ejecucion){
+    proceso Proceso;
+    Proceso.id_proceso = 0;
+    Proceso.tamanio_proceso = tamanio;
+    Proceso.tiempo_ejecucion = tiempo_ejecucion;
+    return 0;
+}
+
+int asignar_marcos(proceso Proceso, int tamanio_pagina, tabla_paginas Tabla){
+    int cantidad_marcos = ceil(Proceso.tamanio_proceso / tamanio_pagina);
+    for(int i=0; i<cantidad_marcos; i++){
+        Tabla[i].id_proceso_asignado = Proceso.id_proceso;
+    }
+    /*VER LO DE LA FRAGMENTACIÓN INTERNA*/
 }
