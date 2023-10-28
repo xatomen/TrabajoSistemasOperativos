355837170c8976340b320354093fd1f729b49fb1
xatomen <jorgito899@gmail.com>
2023-10-28 17:25:06 -0300

    Actualización 28-10-2023

--- main.c
+++ main.c
@@ -7,9 +7,10 @@
 /*Proceso*/
 struct proceso{
     int id_proceso;
-    int tamanio_proceso; /*En KiloBytes*/
-    float tiempo_ejecucion; /*En unidades de tiempo*/
-    int necesidad_marcos;
+    long double tamanio_proceso; /*En KiloBytes*/
+    long double tiempo_ejecucion; /*En unidades de tiempo*/
+    long double necesidad_marcos;
+    long double marcos_asignados;
     struct proceso *next;
 };
 
@@ -25,11 +26,16 @@ typedef marco_pagina tabla_paginas[2048];
 // typedef proceso cola_procesos[500];
 
 /*Prototipo de funciones*/
-int seleccionar_unidad_memoria();
-int necesidad_marco(struct proceso *Proceso, int tamanio_marco);
-int crear_proceso(struct proceso **Proceso, int tamanio_marco);
+long double seleccionar_unidad_memoria();
+long double necesidad_marco(struct proceso *Proceso, long double tamanio_marco);
+int crear_proceso(struct proceso **Proceso, long double tamanio_marco);
+void agregar_proceso_cola(struct proceso **Proceso, struct proceso *NuevoProceso);
 void imprimir_procesos(struct proceso *Proceso);
-int tamano_memoria_fisica();
+long double tamano_memoria_fisica();
+long double tamano_marco_pagina();
+long double necesidad_bloques(struct proceso *Proceso);
+int asignar_bloques(struct proceso *Proceso, long double cantidad_marcos);
+
 
 /*Programa principal*/
 int main(){
@@ -39,8 +45,15 @@ int main(){
     // cola_procesos Cola; /*USAR FCFS???????????????????????*/
 
     /*Ingresamos tamaño de la memoria física y del tamaño de los marcos*/
-    // int memoria_fisica = tamano_memoria_fisica(); /*Tamaño de la memoria física*/
-    int tamanio_marco = 8*1024; /*1KB*/
+    printf("Memoria fisica:\n");
+    long double memoria_fisica = tamano_memoria_fisica(); /*Tamaño de la memoria física*/
+    printf("Tamano marco:\n");
+    long double tamanio_marco = tamano_marco_pagina();
+    long double cantidad_marcos = memoria_fisica/tamanio_marco;
+
+    printf("Tamano memoria fisica: %Lf\n",memoria_fisica);
+    printf("Tamano marco: %Lf\n",tamanio_marco);
+    printf("Cantidad marcos: %Lf\n",cantidad_marcos);
 
     // Tabla->tamanio_marco = tamano_marco;
 
@@ -48,11 +61,12 @@ int main(){
 
     /*Menú y ejecución del aplicativo*/
     while(opcion!=0){
-        printf("Menú:\n");
+        printf("Menu:\n");
         printf("1. Agregar proceso\n");
         printf("2. Imprimir procesos\n");
+        printf("3. Asignar bloques\n");
         printf("0. Salir\n");
-        printf("Ingrese una opción:\n");
+        printf("Ingrese una opcion:\n");
         scanf("%d",&opcion);
         switch(opcion){
             case 0:
@@ -65,6 +79,10 @@ int main(){
             case 2:
                 imprimir_procesos(ColaProcesos);
             break;
+
+            case 3:
+                asignar_bloques(&ColaProcesos,cantidad_marcos);
+            break;
         }
     }
     return 0;
@@ -73,7 +91,7 @@ int main(){
 /*Función que permite seleccionar la unidad de memoria a la hora de crear un proceso*/
 /*Retorna un factor que al multiplicarse por el tamaño de memoria indicado*/
 /*Se podrá obtener la cantidad de memoria en bits*/
-int seleccionar_unidad_memoria(){
+long double seleccionar_unidad_memoria(){
     int opcion = 0;
     L1:
     printf("Seleccione la unidad de memoria:\n");
@@ -102,51 +120,44 @@ int seleccionar_unidad_memoria(){
             case 4:return 8*1024*1024;break;
 
             /*GigaBytes*/
-            // case 5:return 8*1024*1024*1024;break;
+            case 5:return 8*1024*1024*1024;break;
 
             /*TeraBytes*/
-            // case 6:return 8*1024*1024*1024*1024;break;
+            case 6:return 8*1024*1024*1024*1024;break;
 
             /*PetaBytes*/
-            // case 7:return 8*1024*1024*1024*1024*1024;break;
+            case 7:return 8*1024*1024*1024*1024*1024;break;
         }
     }
-    return 0;
 }
 
 /*Función que retorna la necesidad de marcos de un */
-int necesidad_marco(struct proceso *Proceso, int tamanio_marco){
+long double necesidad_marco(struct proceso *Proceso, long double tamanio_marco){
     return ceil(Proceso->tamanio_proceso/tamanio_marco);
 }
 
-int crear_proceso(struct proceso **Proceso, int tamanio_marco){
+int crear_proceso(struct proceso **Proceso, long double tamanio_marco){
     struct proceso *NuevoProceso = malloc(sizeof(struct proceso));
     NuevoProceso->next = NULL;
-    int tamanio_proceso;
-    float tiempo_ejecucion;
+    long double tamanio_proceso;
+    long double tiempo_ejecucion;
 
     /*Ingresamos los datos del proceso a crear*/
-    int factor = seleccionar_unidad_memoria();  /*Cargamos el factor para transformar la memoria del proceso a bits*/
-    printf("Ingrese el tamaño del proceso:\n");
-    scanf("%d",&tamanio_proceso);
+    long double factor = seleccionar_unidad_memoria();  /*Cargamos el factor para transformar la memoria del proceso a bits*/
+    printf("Ingrese el tamano del proceso:\n");
+    scanf("%Lf",&tamanio_proceso);
     printf("Ingrese el tiempo de ejecución del proceso:\n");
-    scanf("%f",&tiempo_ejecucion);
+    scanf("%Lf",&tiempo_ejecucion);
 
     /*Asignamos los datos al proceso creado*/
     NuevoProceso->id_proceso = 0;
     NuevoProceso->tamanio_proceso = tamanio_proceso * factor; /*Tamaño del proceso en bits*/
     NuevoProceso->tiempo_ejecucion = tiempo_ejecucion;
     NuevoProceso->necesidad_marcos = necesidad_marco(NuevoProceso,tamanio_marco);  /*Calculamos cuantos marcos se necesitan para el proceso*/
-    
+    NuevoProceso->marcos_asignados = 0;
+
     /*Insertamos el proceso a la cola de procesos*/
     agregar_proceso_cola(&(*Proceso),NuevoProceso);
-    // if(*Proceso == NULL){
-    //     *Proceso = NuevoProceso;
-    // }
-    // else{
-    //     // crear_proceso(&(*Proceso)->next,tamanio_marco);
-    //     *Proceso->next = NuevoProceso;
-    // }
 
     return 0;
 }
@@ -158,36 +169,69 @@ void agregar_proceso_cola(struct proceso **Proceso, struct proceso *NuevoProceso
     }
     else{
         agregar_proceso_cola(&(*Proceso)->next,NuevoProceso);
-        // *Proceso->next = NuevoProceso;
     }
 }
 
-void imprimir_procesos(struct proceso *Proceso)
-{
-	while (Proceso != NULL)
-	{
+void imprimir_procesos(struct proceso *Proceso){
+	while (Proceso != NULL){
         printf("---------------------------\n");
 		printf("ID: %d\n", Proceso->id_proceso);
-		printf("Tamaño: %d\n", Proceso->tamanio_proceso);
-        printf("Tiempo de ejecución: %f\n", Proceso->tiempo_ejecucion);
-        printf("Necesidad de marcos: %d\n", Proceso->necesidad_marcos);
+		printf("Tamaño: %Lf\n", Proceso->tamanio_proceso);
+        printf("Tiempo de ejecucion: %Lf\n", Proceso->tiempo_ejecucion);
+        printf("Necesidad de marcos: %Lf\n", Proceso->necesidad_marcos);
+        printf("Marcos asignados: %Lf\n", Proceso->marcos_asignados);
         printf("---------------------------\n");
         Proceso = Proceso->next;
 	}
 }
 
-int tamano_memoria_fisica(){
-    int factor = seleccionar_unidad_memoria();
-    int tamano = 0;
-    printf("Ingrese el tamaño del marco:\n");
-    scanf("%d", &tamano);
+long double tamano_memoria_fisica(){
+    long double factor = seleccionar_unidad_memoria();
+    long double tamano = 0;
+    printf("Ingrese el tamano de la memoria fisica:\n");
+    scanf("%Lf", &tamano);
     return tamano * factor;
 }
 
-// int asignar_marcos(struct proceso *Proceso, int tamanio_pagina, tabla_paginas Tabla){
-//     int cantidad_marcos = floor(Proceso.tamanio_proceso / tamanio_pagina);
-//     for(int i=0; i<cantidad_marcos; i++){
-//         Tabla[i].id_proceso_asignado = Proceso.id_proceso;
-//     }
-//     /*VER LO DE LA FRAGMENTACIÓN INTERNA*/
-// }
+long double tamano_marco_pagina(){
+    long double factor = seleccionar_unidad_memoria();
+    long double tamano = 0;
+    printf("Ingrese el tamano del marco:\n");
+    scanf("%Lf", &tamano);
+    return tamano * factor;
+}
+
+/*VER ESTO!!!!!!!*/
+long double necesidad_bloques(struct proceso *Proceso){
+    printf("funcion necesidad bloques\n");
+    long double necesidad = 0;
+    printf("while\n");
+    while (Proceso != NULL){
+        printf("iteracion\n");
+        necesidad = necesidad + Proceso->necesidad_marcos;
+        printf("necesidad: %Lf\n",necesidad);
+        printf("necesidad bloques proceso: %Lf",Proceso->necesidad_marcos);
+        system("pause");
+        Proceso = Proceso->next;
+        
+	}
+    return necesidad;
+}
+
+int asignar_bloques(struct proceso *Proceso, long double cantidad_marcos){
+    
+    printf("declaracion\n");
+    long double asignacion_proporcional;
+    printf("funcion\n");
+    long double total_necesidad = necesidad_bloques(Proceso);
+
+    printf("while\n");
+    while (Proceso != NULL){
+        asignacion_proporcional = (Proceso->necesidad_marcos/total_necesidad)*cantidad_marcos;
+        if(asignacion_proporcional<1){Proceso->necesidad_marcos = 1;}
+        else{Proceso->necesidad_marcos = floor(asignacion_proporcional);}
+        Proceso = Proceso->next;
+	}
+
+    /*VER LO DE LA FRAGMENTACIÓN INTERNA*/
+}
